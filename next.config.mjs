2d66071f4ced5bce71e4e17c29a8c83819a6dbb3
Rabin Lamichhane <64152903+EV-OD@users.noreply.github.com>
2024-10-21 16:26:38 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
+    basePath: "/simple-form-website",
 };
 
 export default nextConfig;
