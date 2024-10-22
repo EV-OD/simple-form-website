718cfffee9fda03c2f0770841b011a6b175a9703
EV-OD <evod599@gmail.com>
2024-10-22 08:07:00 +0545

    Refactor firebase.ts to log firebaseConfig

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,6 +14,8 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(firebaseConfig)
+
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
